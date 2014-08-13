18b48cd9638ee29d72565d2b4f032f6c88b8b6b2
Manu Mehrotra <septembermanu@gmail.com>
2014-08-13 02:06:59 -0700

    removing unnecessary imports

--- src/main/java/com/nowoncloud/fizzbuzz/scheduler/worker/FizzBuzzCallWorkerImpl.java
+++ src/main/java/com/nowoncloud/fizzbuzz/scheduler/worker/FizzBuzzCallWorkerImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.nowoncloud.fizzbuzz.domain.FizzBuzzCall;
 import com.nowoncloud.fizzbuzz.service.CallService;
--- src/main/java/com/nowoncloud/fizzbuzz/service/FizzBuzzCallServiceImpl.java
+++ src/main/java/com/nowoncloud/fizzbuzz/service/FizzBuzzCallServiceImpl.java
@@ -14,7 +14,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.nowoncloud.fizzbuzz.domain.FizzBuzzCall;
 import com.nowoncloud.fizzbuzz.repository.CallDao;
