be688ca4bfe75afae3ebc5c9c6927234a36b2404
Manu Mehrotra <septembermanu@gmail.com>
2014-08-13 01:59:32 -0700

    making FizzBuzzCallWorkerImpl transactional

--- src/main/java/com/nowoncloud/fizzbuzz/scheduler/worker/FizzBuzzCallWorkerImpl.java
+++ src/main/java/com/nowoncloud/fizzbuzz/scheduler/worker/FizzBuzzCallWorkerImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.nowoncloud.fizzbuzz.domain.FizzBuzzCall;
 import com.nowoncloud.fizzbuzz.service.CallService;
@@ -12,6 +13,7 @@ import com.twilio.sdk.TwilioRestException;
 
 
 @Component("callWorker")
+@Transactional
 public class FizzBuzzCallWorkerImpl implements Worker {
 	private static final Logger logger = Logger.getLogger(FizzBuzzCallWorkerImpl.class);
 	@Autowired
